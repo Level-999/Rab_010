949383e96550f1d4d3729990c29ef17cea36c6f6
Level-999 <dalsep@mail.ru>
2022-01-06 15:46:50 +1000

    Добавьте файлы проекта.

---
+++ Rab_010/Class Rab 10.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rab_010
+{
+    internal class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        //double a = Math.PI / 180;        
+              
+        public bool isCorrect = true;
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    gradus = value % 360;
+                }
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Gradus += value / 60;
+                    min = value % 60;
+                }
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Min += value / 60;
+                    sec = value % 60;
+                }
+            }
+        }
+
+        public Angle(int gradus, int min, int sec )
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+
+        }
+
+        public int ToRadians()
+        {
+
+            return (((gradus + min + sec) * (int)Math.PI / 180));
+
+        }
+    }
+}
---
+++ Rab_010/Rab 10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rab_010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд.
+            //Реализовать класс, в котором указанные значения представлены в виде свойств.
+            //Для свойств предусмотреть проверку корректности данных.
+            //Класс должен содержать конструктор для установки начальных значений,
+            //а также метод ToRadians для перевода угла в радианы.
+            //Создать объект на основе разработанного класса. Осуществить использование объекта в программе.
+            
+            Angle angle;
+            do
+            {
+                Console.WriteLine("Ввод данных, задаём угол");
+                int gradus1 = Convert.ToInt32(Console.ReadLine());
+                int min1 = Convert.ToInt32(Console.ReadLine());
+                int sec1 = Convert.ToInt32(Console.ReadLine());
+
+                angle = new Angle(gradus1, min1, sec1);
+            }
+           
+
+            while (angle.isCorrect == false);
+            int totalgradus = angle.ToRadians();
+
+            Console.WriteLine(totalgradus);
+            Console.ReadKey();
+        }
+    }
+}
